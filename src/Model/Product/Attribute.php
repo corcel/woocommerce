a0ba45eab89bb90d57780b03c26bbbd42c7db96a
Dartui <krzysztof.grabania@gmail.com>
2017-08-26 23:04:49 +0200

    Product attributes aliases

--- src/Model/Product/Attribute.php
+++ src/Model/Product/Attribute.php
@@ -3,9 +3,24 @@
 namespace Corcel\WooCommerce\Model\Product;
 
 use Corcel\Model;
+use Corcel\Traits\AliasesTrait;
 
 class Attribute extends Model
 {
+    use AliasesTrait;
+
+    /**
+     * @var array
+     */
+    protected static $aliases = [
+        'id'       => 'attribute_id',
+        'slug'     => 'attribute_name',
+        'name'     => 'attribute_label',
+        'type'     => 'attribute_type',
+        'order_by' => 'attribute_orderby',
+        'public'   => 'attribute_public',
+    ];
+
     /**
      * @var array
      */
